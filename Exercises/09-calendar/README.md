70952ba9168f734bc1456467fdb9c2b1c2b46172
David Dokic <david.dokic28@gmail.com>
2023-01-22 15:08:55 +0100

    Caledar MERN auth finished

---
+++ Exercises/09-calendar/README.md
@@ -0,0 +1,17 @@
+# Calendar APP
+
+
+
+
+## Developement steps
+
+1. Rename .env.template to .env 
+2. Make the necessary changes to the enviroment variables
+
+### env mockup 
+
+```
+VITE_API_URL=http://localhost:400/api
+
+
+```
--- Exercises/09-calendar/src/auth/pages/LoginPage.jsx
+++ Exercises/09-calendar/src/auth/pages/LoginPage.jsx
@@ -1,19 +1,85 @@
 
 
+import { useEffect } from 'react';
+import Swal from 'sweetalert2';
+import { useAuthStore, useForm } from '../../hooks';
 import './LoginPage.css';
 
+
+const loginFormFields = {
+    loginEmail: '',
+    loginPassword: ''
+}
+
+const registerFormFields = {
+    registerName: '',
+    registerEmail: '',
+    registerPassword: '',
+    registerPassword2: '',
+}
+
+
+
+
+
+
 export const LoginPage = () => {
+
+    const { startLogin, errorMessage, startRegister } = useAuthStore();
+
+    const {loginEmail, loginPassword, onInputChange:onLoginInputChange } = useForm( loginFormFields );
+    
+    const {registerName, registerEmail, registerPassword, registerPassword2, onInputChange:onRegisterInputChange } = useForm( registerFormFields );
+
+    const loginSubmit = (event) => {
+        event.preventDefault();
+        startLogin({ email: loginEmail, password: loginPassword})
+        
+    }
+
+
+    const registerSubmit = (event) => {
+        event.preventDefault()
+
+        // Validaciones
+
+        if(registerPassword !== registerPassword2){
+            Swal.fire('Error en el registro', 'Las contraseñas deben coincidir', 'error' )
+            return 
+        }
+
+        // Dispatch
+
+        startRegister({ name: registerName, email: registerEmail, password: registerPassword })
+        
+    }
+
+
+    useEffect(() => {
+      if(errorMessage !== undefined){
+        Swal.fire('Error en la autenticación', errorMessage, 'error')
+      }
+ 
+    }, [errorMessage])
+    
+
+
+
+    
     return (
         <div className="container login-container">
             <div className="row">
                 <div className="col-md-6 login-form-1">
                     <h3>Ingreso</h3>
-                    <form>
+                    <form onSubmit={loginSubmit}>
                         <div className="form-group mb-2">
                             <input 
                                 type="text"
                                 className="form-control"
                                 placeholder="Correo"
+                                name="loginEmail"
+                                value={loginEmail}
+                                onChange={onLoginInputChange}
                             />
                         </div>
                         <div className="form-group mb-2">
@@ -21,6 +87,9 @@ export const LoginPage = () => {
                                 type="password"
                                 className="form-control"
                                 placeholder="Contraseña"
+                                name="loginPassword"
+                                value={loginPassword}
+                                onChange={onLoginInputChange}
                             />
                         </div>
                         <div className="form-group mb-2">
@@ -35,12 +104,15 @@ export const LoginPage = () => {
 
                 <div className="col-md-6 login-form-2">
                     <h3>Registro</h3>
-                    <form>
+                    <form onSubmit={registerSubmit}>
                         <div className="form-group mb-2">
                             <input
                                 type="text"
                                 className="form-control"
                                 placeholder="Nombre"
+                                name="registerName"
+                                value={registerName}
+                                onChange={onRegisterInputChange}
                             />
                         </div>
                         <div className="form-group mb-2">
@@ -48,6 +120,9 @@ export const LoginPage = () => {
                                 type="email"
                                 className="form-control"
                                 placeholder="Correo"
+                                name="registerEmail"
+                                value={registerEmail}
+                                onChange={onRegisterInputChange}
                             />
                         </div>
                         <div className="form-group mb-2">
@@ -55,6 +130,9 @@ export const LoginPage = () => {
                                 type="password"
                                 className="form-control"
                                 placeholder="Contraseña" 
+                                name="registerPassword"
+                                value={registerPassword}
+                                onChange={onRegisterInputChange}
                             />
                         </div>
 
@@ -63,6 +141,9 @@ export const LoginPage = () => {
                                 type="password"
                                 className="form-control"
                                 placeholder="Repita la contraseña" 
+                                name="registerPassword2"
+                                value={registerPassword2}
+                                onChange={onRegisterInputChange}
                             />
                         </div>
 
